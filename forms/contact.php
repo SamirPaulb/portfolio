7512935a5d108725c72e24b7e9e293a5034738a7
Samir Paul <77569653+SamirPaul1@users.noreply.github.com>
2022-12-09 18:51:42 +0000

    --

--- forms/contact.php
+++ forms/contact.php
@@ -2,7 +2,7 @@
  
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'paul.samir.2002@gmail.com';
+  $receiving_email_address = 'samirpaulb@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
